f82bc4373e0709e9d2af4c54cd694a69571925f7
phonghoang@gmail.com <phonghoang@gmail.com>
2015-08-16 18:40:06 -0700

    track added and cars animate

--- racecar.js
+++ racecar.js
@@ -1,21 +1,64 @@
+var playerOneWin = "";
+var playerTwoWin = "";
+
 $(document).ready(function(){
 
-  $('html').keypress(function (e) {
+/*========================================
+          PLAYER ONE SETTINGS
+=======================================*/
+
+  var functionOne = $('html').keypress(function (e) {
     if (e.keyCode == 97) {
-      $(".playerOne").animate({ "left": "+=200px" }, 100 );}
+      $(".playerOne").animate({ "left": "+=20px" }, 0 );}
   });
+
+/*========================================
+          PLAYER TWO SETTINGS
+=======================================*/
 
   $('html').keypress(function (e) {
     if (e.keyCode == 108) {
-      $(".playerTwo").animate({ "left": "+=200px" }, 100 );}
+      $(".playerTwo").animate({ "left": "+=20px" }, 0 );}
   });
 
-  $("input").click(function(){
-    $(".playerOne,.playerTwo").removeAttr("style");
+/*========================================
+                WINNER
+=======================================*/
+
+  $('html').keypress( function(event) {     
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+      if(keycode == '97'){
+        $('#html').html(playerOneWin++);  
+      }
+      if (playerOneWin === 64) {
+        alert("PLAYER ONE WINS");
+      }
+   });
+
+  $('html').keypress( function(event) {     
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+      if(keycode == '108'){
+        $('#html').html(playerTwoWin++);  
+      }
+      if (playerTwoWin === 64) {
+        alert("PLAYER TWO WINS");
+      } 
+   });
+
+/*========================================
+            RESET GAME
+=======================================*/
+
+ $("#reset").click(function(){
+    location.reload();
   });
-
-
-
-
-
 })
+
+/*
+$('#right').click(function () {
+    var leftPos = $('div.outer_container').scrollLeft();
+    $("div.outer_container").animate({
+        scrollLeft: leftPos + 100
+    }, 800);
+});
+*/
